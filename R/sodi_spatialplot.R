a813d40ec861ce7eabbb16b151b2c35638c89807
Noam Ross <noam.ross@gmail.com>
2013-09-24 13:51:55 -0700

    Plot function tweaked for new parameter setup

--- R/sodi_spatialplot.R
+++ R/sodi_spatialplot.R
@@ -6,15 +6,16 @@
 sodi_spatialplot = function(sodi, times) {
   parms = attr(sodi, "parms")
   max_infections = max(sodi$Infections)  
+  n_stages = max(parms$sp_stages)
   
   matched_times = match_times(sodi, times)
   
   plot = ggplot(subset(sodi, Time %in% matched_times),
-                aes(x=X, y=Y, size=Stage, col=Infections)) + 
+                aes(x=X, y=Y, size=Stage, col=Infections, shape=Species)) + 
            geom_point() + coord_fixed() + 
            scale_alpha(range=c(0.1,1), limits=c(0,1), breaks=NULL) +
-           scale_size(range=c(2,4), limits=c(1,parms$n_stages),
-                      breaks=c(1,parms$n_stages)) +
+           scale_size(range=c(2,n_stages+1), limits=c(1,n_stages),
+                      breaks=1:n_stages) +
            xlim(parms$bbox[1:2]) + ylim(parms$bbox[3:4]) +
            scale_color_gradientn(limits=c(0,max_infections),
                                  colours=c("#408000", "#FFCC66", "#800080"),
